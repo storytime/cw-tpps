b587650f65ea046cdc412f83a7278a0b1e729adf
Sergey <neecman@mail.ua>
2013-01-09 03:56:38 +0200

    refactoring titul page

--- grails-app/services/cw/MailSenderService.groovy
+++ grails-app/services/cw/MailSenderService.groovy
@@ -2,14 +2,10 @@ package cw
 
 class MailSenderService {
 
-   /* def sendMail(String mail, String subject, String body) {
+   def sendMail(String mail, String subject, String body) {
 		def sc = new MailSenderController()
 		sc.sendMail(mail,subject,body) 
-    }*/
-	def sendMail() {
-		def sc = new MailSenderController()
-		sc.sendMail("neecman@mail.ru","Hello Serg","How are you?")
-			}
-	
+    }
+
 	
 }
